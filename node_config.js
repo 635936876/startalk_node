a33486be33428ad71a2e06622804b5aef1bf6f93
董超 <chaos.@qunar.com>
2019-09-23 10:06:46 +0800

    修改二级菜单为由后台获取

--- node_config.js
+++ node_config.js
@@ -1,29 +1,29 @@
 module.exports = {
     "javaurl": "http://100.80.129.108:8080/package",
-    "fileurl": "http://100.80.129.108:8080",
+    "fileurl": "http://100.80.129.24:8080",
     "httpurl": "http://100.80.129.108:8080/newapi",
     "apiurl": "http://100.80.129.108:8080/api",
     "xmpp": "100.80.129.108" + ":5280",
     "web":{
         "title":'Qchat Web',
-        "webcss": '/styles/web/page/' + 'qchat@d80c43acf9053f2dad2e72268fddc86f.css',
-        "webjs": '/scripts/page/web/' + 'qchat@6be5ffdaa0167d216a7bcffa4df033b2.js'
+        "webcss": '/styles/web/page/' + 'qchat@68d0532136740fac3a145d05bad036cc.css',
+        "webjs": '/scripts/page/web/' + 'qchat@caf3d1bb7ab2c7b65dd3ef592608cef4.js'
     },
     "touch":{
         "title":'Qchat Touch',
         "touchcss": '/styles/touch/page/' + 'qchat@268bda38e7c91e086867bf7f0da484fd.css',
-        "touchjs": '/scripts/page/touch/' + 'qchat@7f1e709b0f82bc095e60c28fc41350a9.js'
+        "touchjs": '/scripts/page/touch/' + 'qchat@b2448bac1dde573a833830b174b097e3.js'
     },
     "touchSubmenu":{
         "title":'Callcenter咨询保修系统服务目录',
         "touchsubmenucss": '/styles/submenu/' + 'touchSubmenu@2e24653dd36db9f99df632a0626a8ef9.css',
-        "touchsubmenujs": '/scripts/page/submenu/' + 'touchSubmenu@3d2ebea4872cef6d343377f36ec5f977.js'
+        "touchsubmenujs": '/scripts/page/submenu/' + 'touchSubmenu@8650231bce15ef43295a29444417a233.js'
     },
     "webSubmenu":{
         "title":'Callcenter咨询保修系统服务目录',
-        "websubmenucss": '/styles/submenu/' + 'webSubmenu@c81ba9775b4ebd87325de5ed6e58dd10.css',
-        "websubmenujs": '/scripts/page/submenu/' + 'webSubmenu@25615a10fe9c2bc3a48265bf6913641d.js'
+        "websubmenucss": '/styles/submenu/' + 'webSubmenu@345cc633de1ea544e631d3c94c7736bb.css',
+        "websubmenujs": '/scripts/page/submenu/' + 'webSubmenu@c7e96fd112b539d760426af778c37e2a.js'
     },
     "jquery": '/scripts/lib/jquery/'+ 'jquery_1.7@88c8777bbcb1fb174b5edf2c1dd03278.js',
-    "navigation": '/' + "navigation@7eae430327ad0b3f232811d4e01c0856.js"
+    "navigation": '/' + "navigation@d17692dc7e050e06bb5332bf057f4a99.js"
 }
